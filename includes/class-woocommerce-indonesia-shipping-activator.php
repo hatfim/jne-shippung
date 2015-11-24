c4dfc38362e0d48991ef0fc27487e12b3dc9d417
Hatfim <hatfim@Hatfims-MacBook-Pro.local>
2015-11-24 16:12:49 +0700

    add plugins boilerplate

---
+++ includes/class-woocommerce-indonesia-shipping-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       hatfim.ydniw.com
+ * @since      1.0.0
+ *
+ * @package    Woocommerce_Indonesia_Shipping
+ * @subpackage Woocommerce_Indonesia_Shipping/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Woocommerce_Indonesia_Shipping
+ * @subpackage Woocommerce_Indonesia_Shipping/includes
+ * @author     Hatfim Ydniw <hatfim@ydniw.com>
+ */
+class Woocommerce_Indonesia_Shipping_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-woocommerce-indonesia-shipping-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       hatfim.ydniw.com
+ * @since      1.0.0
+ *
+ * @package    Woocommerce_Indonesia_Shipping
+ * @subpackage Woocommerce_Indonesia_Shipping/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Woocommerce_Indonesia_Shipping
+ * @subpackage Woocommerce_Indonesia_Shipping/includes
+ * @author     Hatfim Ydniw <hatfim@ydniw.com>
+ */
+class Woocommerce_Indonesia_Shipping_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
